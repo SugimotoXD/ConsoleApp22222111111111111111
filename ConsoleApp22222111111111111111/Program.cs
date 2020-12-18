84cd59a05a0dea21ccd66f931ba0917e3d7cc863
fsafdsa Lfybkf <P.danila228@mail.ru>
2020-12-18 18:25:27 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp22222111111111111111/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22222111111111111111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("entet a:");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("enter b:");
+                int b = int.Parse(Console.ReadLine());
+                Console.Write("enter c:");
+                int c = int.Parse(Console.ReadLine());
+                bool x = (a < 45) ^ (b < 45) ^ (c < 45);
+
+                Console.WriteLine(x);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
